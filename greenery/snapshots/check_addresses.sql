07a1a2907c53e530a99bcbd95f3550f3745324ff
tnakamoto-hbo <thomas.nakamoto@warnermedia.com>
2021-11-22 15:41:42 +0000

    add snapshots

---
+++ greenery/snapshots/check_addresses.sql
@@ -0,0 +1,8 @@
+{ % snapshot addresses_snapshot % } { { config(
+  target_schema = 'snapshots',
+  unique_key = 'address_id',
+  strategy = 'check',
+  check_cols = ['address', 'zipcode', 'state', 'country']
+) } }
+SELECT *
+FROM { { source('tutorial', 'addresses') } } { % endsnapshot % }
