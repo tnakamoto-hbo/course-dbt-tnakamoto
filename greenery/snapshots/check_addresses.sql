055fb06ec919cb07114c9442c9de60269fa312b0
tnakamoto-hbo <thomas.nakamoto@warnermedia.com>
2021-11-27 22:05:47 +0000

    arrange files

--- greenery/snapshots/check_addresses.sql
+++ greenery/snapshots/check_addresses.sql
@@ -1,8 +1,16 @@
-{ % snapshot addresses_snapshot % } { { config(
-  target_schema = 'snapshots',
-  unique_key = 'address_id',
-  strategy = 'check',
-  check_cols = ['address', 'zipcode', 'state', 'country']
-) } }
-SELECT *
-FROM { { source('tutorial', 'addresses') } } { % endsnapshot % }
+{% snapshot addresses_snapshot %} 
+
+  {{ 
+    config(
+      target_schema='snapshots',
+      unique_key='address_id',
+
+      strategy='check',
+      check_cols=['address', 'zipcode', 'state', 'country']
+    ) 
+  }}
+
+  SELECT *
+  FROM {{ source('tutorial', 'addresses') }} 
+
+{% endsnapshot %}
